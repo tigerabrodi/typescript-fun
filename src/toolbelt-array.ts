1ff722fcf5900ac85e83491a43ea8add76c193ed
Tiger Abrodi <tigerabrodi@gmail.com>
2023-12-12 12:05:42 +0100

    length

--- src/toolbelt-array.ts
+++ src/toolbelt-array.ts
@@ -8,12 +8,15 @@ type Reverse<T extends any[]> = T extends [infer First, ...infer Rest]
   ? [...Reverse<Rest>, First]
   : T;
 
+type LengthArr<T extends any[]> = T["length"];
+
 // Example usages
 type TestAppend = Append<[1, 2, 3], 4>; // Result: [1, 2, 3, 4]
 type TestPrepend = Prepend<[2, 3, 4], 1>; // Result: [1, 2, 3, 4]
 type TestConcat = Concat<[1, 2], [3, 4]>; // Result: [1, 2, 3, 4]
 
 type TestReverse = Reverse<[1, 2, 3]>;
-// type TestLength = Length<[1, 2, 3, 4, 5]>;
+type TestLengthArr = LengthArr<[1, 2, 3, 4, 5]>; // Result: 5
+
 // type TestIncludes = Includes<[1, 2, 3, 4, 5], 3>;
 // type TestFlatten = Flatten<[[1, 2], [3, 4], 5]>;
