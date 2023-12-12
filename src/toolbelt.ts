04486f28373048660a7c5a8caf897cf46415007d
Tiger Abrodi <tigerabrodi@gmail.com>
2023-12-12 11:39:18 +0100

    lowercase

--- src/toolbelt.ts
+++ src/toolbelt.ts
@@ -26,16 +26,25 @@ type Includes<
   ? true
   : false;
 
+type UpperCase<S extends string> = S extends `${infer First}${infer Rest}`
+  ? `${Uppercase<First>}${UpperCase<Rest>}`
+  : S;
+
+type LowerCase<S extends string> = S extends `${infer First}${infer Rest}`
+  ? `${Lowercase<First>}${LowerCase<Rest>}`
+  : S;
+
 // Example usages
 type TestSplit = Split<"hello-world", "-">; // Result: ['hello', 'world']
 
-type TestReplace = Replace<"TypeScript", "Script", "Toolbelt">;
+type TestReplace = Replace<"TypeScript", "Script", "Toolbelt">; // Result: 'TypeToolbelt'
 
-type TestLength = Length<"hello">;
+type TestLength = Length<"hello">; // Result: 5
 
-type TestIncludes = Includes<"hello world", "world">;
+type TestIncludes = Includes<"hello world", "world">; // Result: true
 
-// type TestUpperCase = UpperCase<'typescript'>;
-// type TestLowerCase = LowerCase<'TYPESCRIPT'>;
+type TestUpperCase = UpperCase<"typescript">; // Result: 'TYPESCRIPT'
+
+type TestLowerCase = LowerCase<"TYPESCRIPT">; // Result: 'typescript'
 // type TestStartsWith = StartsWith<'hello world', 'hello'>;
 // type TestEndsWith = EndsWith<'hello world', 'world'>;
