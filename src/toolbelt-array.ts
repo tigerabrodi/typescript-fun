aede5a062a92f51ea112b55f61c8faf32219a8fb
Tiger Abrodi <tigerabrodi@gmail.com>
2023-12-12 11:59:23 +0100

    append

--- src/toolbelt-array.ts
+++ src/toolbelt-array.ts
@@ -1,1 +1,10 @@
+type Append<T extends any[], U> = [...T, U];
 
+// Example usages
+type TestAppend = Append<[1, 2, 3], 4>;
+// type TestPrepend = Prepend<[2, 3, 4], 1>;
+// type TestConcat = Concat<[1, 2], [3, 4]>;
+// type TestReverse = Reverse<[1, 2, 3]>;
+// type TestLength = Length<[1, 2, 3, 4, 5]>;
+// type TestIncludes = Includes<[1, 2, 3, 4, 5], 3>;
+// type TestFlatten = Flatten<[[1, 2], [3, 4], 5]>;
