0ecd7eb06a530b44e44deb5ea3492da4929ff55d
Tiger Abrodi <tigerabrodi@gmail.com>
2023-12-12 09:36:20 +0100

    element type of array

--- src/infer.ts
+++ src/infer.ts
@@ -1,3 +1,4 @@
+// return type of function
 type ReturnTypeOf<T> = T extends (...args: any[]) => infer R ? R : never;
 
 function greet(name: string) {
@@ -6,3 +7,11 @@ function greet(name: string) {
 
 type GreetingReturnType = ReturnTypeOf<typeof greet>;
 const greeting: GreetingReturnType = greet("Alice");
+
+// Element type of an array
+type ElementType<T> = T extends Array<infer U> ? U : never;
+
+const numberArray = [1, 2, 3];
+
+type NumberArrayElement = ElementType<typeof numberArray>;
+const firstElement: NumberArrayElement = numberArray[0];
